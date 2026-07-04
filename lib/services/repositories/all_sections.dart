e7e22ea9c2b3af49f1e84cee644f929f71b89d08
OakFoil <elsennahway@gmail.com>
2026-07-04 16:41:05 +0300

    fix data retrival from backend other than web

--- lib/services/repositories/all_sections.dart
+++ lib/services/repositories/all_sections.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
 import 'package:infinityfree_bypasser/infinityfree_bypasser.dart';
 import 'package:nu_sched_gen/models/section.dart';
@@ -15,19 +15,19 @@ class AllSections extends _$AllSections {
   @override
   Future<Map<String, Set<Section>>> build() async {
     const url = "https://nu-courses.rf.gd",
-        corsFixUrl = "https://proxy.corsfix.com/?" + url;
+        corsFixUrl = "https://proxy.corsfix.com/?$url",
+        usedUrl = kIsWeb ? corsFixUrl : url;
 
-    await _bypasser.bypass(corsFixUrl);
+    await _bypasser.bypass(usedUrl);
 
     if (_bypasser.cookie == null) {
       return Future.error("InfinityFree Bot Check Bypass Failed");
     }
 
+    final header = {'Cookie': _bypasser.cookie!};
     final Response(:body) = await get(
-      Uri.parse(corsFixUrl),
-      headers: {
-        "x-corsfix-headers": json.encode({'Cookie': _bypasser.cookie!}),
-      },
+      Uri.parse(usedUrl),
+      headers: kIsWeb ? {"x-corsfix-headers": json.encode(header)} : header,
     );
     final List<dynamic> data = json.decode(body)["data"];
     final Set<Slot> slots = data.map((a) => Slot.fromJson(a)).toSet();
