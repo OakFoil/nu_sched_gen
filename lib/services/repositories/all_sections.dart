ec01d728ea0512f0cdd6ed24d5bd1b4d1db64cad
OakFoil <elsennahway@gmail.com>
2026-07-20 09:27:51 +0300

    remove infinityfree_bypasser

--- lib/services/repositories/all_sections.dart
+++ lib/services/repositories/all_sections.dart
@@ -1,28 +1,16 @@
 import 'dart:convert';
 import 'package:http/http.dart';
-import 'package:infinityfree_bypasser/infinityfree_bypasser.dart';
 import 'package:nu_sched_gen/models/section.dart';
 import 'package:nu_sched_gen/models/slot.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'all_sections.g.dart';
 
-final InfinityfreeBypasser _bypasser = InfinityfreeBypasser();
-
 @Riverpod(keepAlive: true)
 Future<Map<String, Set<Section>>> allSections(Ref ref) async {
   const url = "https://sched-gen.rf.gd/api";
 
-  await _bypasser.bypass(url);
-
-  if (_bypasser.cookie == null) {
-    return Future.error("InfinityFree Bot Check Bypass Failed");
-  }
-
-  final Response(:body) = await get(
-    Uri.parse(url),
-    headers: {'Cookie': _bypasser.cookie!},
-  );
+  final Response(:body) = await get(Uri.parse(url));
   final List<dynamic> data = json.decode(body)["data"];
   final Set<Slot> slots = data.map((a) => Slot.fromJson(a)).toSet();
 
